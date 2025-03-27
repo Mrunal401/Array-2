81da0dc24b5ed7c70c1d3b0f0260952991543fde
Mrunal401 <52656959+Mrunal401@users.noreply.github.com>
2025-03-27 15:11:14 -0700

    completed prob1

---
+++ DissapearedNumbers.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        int n = nums.length;
+
+        for(int i = 0; i<n; i++){
+            int num = nums[i];
+            int idx = Math.abs(nums[i]) - 1;
+            
+            if(nums[idx] > 0){
+                nums[idx] *= -1;
+            }
+        }
+
+        List<Integer> result = new ArrayList<>();
+
+        for(int i = 0; i<n;i++){
+            if(nums[i] > 0){
+                result.add(i+1);
+            } else{
+                nums[i] *= -1;
+            }
+        }
+        return result;
+    }
+}
